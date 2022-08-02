9cb4693c3b87036aab4b52f1e1e5a7228bc95ecd
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-08-03 01:47:13 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function introduction (name){
+    return `Hi, my name is ${name}`;
+}
+
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program using ${language}`;
+}
+function introductionWithLanguageOptional(name, language){
+    return `JavaScript: ${name} JavaScript: ${language}`;
+}
